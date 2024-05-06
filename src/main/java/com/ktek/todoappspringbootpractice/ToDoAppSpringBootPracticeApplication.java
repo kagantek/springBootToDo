e558cda9fe3c942b3f9c29b0a00f9c4f6f2c3f22
kagantek <kagantek2003@hotmail.com>
2024-05-06 19:39:27 +0300

    First commit to set up project files.

---
+++ src/main/java/com/ktek/todoappspringbootpractice/ToDoAppSpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ktek.todoappspringbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppSpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppSpringBootPracticeApplication.class, args);
+	}
+
+}
